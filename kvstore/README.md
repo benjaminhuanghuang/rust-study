44e63098efcb784dfb6395c3ff263b64ec7ccb14
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-06-22 13:05:29 -0700

    update

---
+++ kvstore/README.md
@@ -0,0 +1,6 @@
+# Introduction to Rust Part 1 & Part 2
+
+https://www.youtube.com/watch?v=lLWchWTUFOQ&ab_channel=RyanLevickRyanLevick
+
+https://github.com/rylev/intro-to-rust-streams
+
--- kvstore/src/main.rs
+++ kvstore/src/main.rs
@@ -1,9 +1,9 @@
-use std::str::SplitN;
 use core::iter::Skip;
 use std::collections::HashMap;
 use std::env::Args;
 use std::fs;
 use std::io;
+use std::str::SplitN;
 
 fn main() {
   // let key = std::env::args().nth(1).expect("Please specify an key");
@@ -12,15 +12,23 @@ fn main() {
   let mut arguments: Skip<Args> = std::env::args().skip(1);
   let key: String = arguments.next().expect("Key was not there");
   let value: String = arguments.next().unwrap();
+  println!("The key is '{}' and the value is '{}'", key, value);
 
   let contents: String = format!("{}\t{}\n", key, value);
   std::fs::write("kv.db", contents).unwrap();
 
-  let database:Result<Database, io::Error> = Database::new();
+  let mut database = Database::new().expect("Creating db failed");
+  database.insert(key.to_uppercase(), value.clone());
+  database.insert(key, value);
+  match database.flush() {
+    Ok(()) => println!("YAY!"),
+    Err(err) => println!("OH NOS! Error! {}", err),
+  }
 }
 
 struct Database {
   map: HashMap<String, String>,
+  flush: bool,
 }
 
 impl Database {
@@ -35,18 +43,46 @@ impl Database {
     //   }
     // }
     let contents: String = fs::read_to_string("kv.db")?;
-    for line in contents.lines() {   // borrowing the string owned by contents
-      let mut chunks: SplitN<char> = line.splitn(2,'\t');  // get an interator
-      let key : &str = chunks.next().expect("No key");
-      let value : &str = chunks.next().expect("No value");
+    for line in contents.lines() {
+      // borrowing the string owned by contents
+      let mut chunks: SplitN<char> = line.splitn(2, '\t'); // get an interator
+      let key: &str = chunks.next().expect("No key");
+      let value: &str = chunks.next().expect("No value");
 
       map.insert(key.to_owned(), value.to_owned());
-
     }
     // parse the file
-
     Ok(Database {
-      map: HashMap::new(),
+      map: map,
+      flush: false,
     })
   }
+
+  fn insert(&mut self, key: String, value: String) {
+    self.map.insert(key, value);
+  }
+
+  fn flush(&self) -> std::io::Result<()> {
+    do_flush(&self)
+  }
+}
+
+impl Drop for Database {
+  fn drop(&mut self) {
+    if !self.flush {
+      let _ = do_flush(self);
+    }
+  }
+}
+
+fn do_flush(database: &Database) -> std::io::Result<()> {
+  println!("Do flush called");
+  let mut contents = String::new();
+  for (key, value) in &database.map {
+    contents.push_str(key);
+    contents.push('\t');
+    contents.push_str(&&&&value);
+    contents.push('\n');
+  }
+  std::fs::write("kv.db", contents)
 }
