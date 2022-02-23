3002c9d2b46f3f26d3357d71901c5ad5eab905bf
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2022-02-23 09:14:13 -0800

    update

--- macro/macro_demo/src/main.rs
+++ macro/macro_demo/src/main.rs
@@ -1,3 +1,21 @@
+#![allow(unused)]
+macro_rules! calculate {
+  // $e 为expr类型, match 1 + 2
+  (eval $e:expr) => {{
+    {
+      let val: usize = $e;
+      println!("{} = {}", stringify!{$e}, val);
+    }
+  }};
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
+  calculate!{
+    eval 1+2
+  }
+  calculate!{
+    eval (1+2) + (3+4)
+  }
 }
