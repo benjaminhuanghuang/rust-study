585b413169508130320990be08f419b1f5009f70
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2022-04-10 14:06:41 -0700

    update

--- #String/README.md
+++ #String/README.md
@@ -1,5 +1,19 @@
+# str, &str, String
 
-# str vs String
+It's all about "who owns the memory".
+
+`String` contains a string in memory and owns the memory for it.
+
+Use `String` for returning strings created within a function or (usually) when storing strings in a struct or enum.
+
+If you have a `String` you can pass a reference to it to convert it to &str.
+
+`&str` is just a reference to another string (slice) but does not own the memory for it.
+
+Prefer `&str` in function arguments to accept string slices and make it clear the function will not mutate the string.
+
+If you have a `&str` and want a new String you can clone it either by to_owned() or to_string() (they are effectively the same - use whichever makes your code clearer to read and consistent). These will copy the memory and make a new String.
+
 ```
 let _val= "hello";                        // primitive string type immutable by default
 
@@ -10,9 +24,7 @@ let _val1= String::from("Hello, World!"); // Growable and mutable
 
 ## Strings
 - UTF-8 Encoded
-
 - Non-Null-Byte terminated
-
 - Not collections of chars
 
 String in memory
--- #String/string-demo/src/main.rs
+++ #String/string-demo/src/main.rs
@@ -1,10 +1,43 @@
 fn main() {
-    let text = "hello";
+    let text:&str = "hello";
 
-    println!("{}", first_line(text));
+    let text:String = String::from("hello");
+    let text:String = "hello".to_string();
+    let text:String = "hello".to_owned();
+
+    let text:&str = &text[..];  // slice of entire string
+
+
+    // 
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    s.replace_range(.., "replaced")
+
+
+    // Concatenate
+    let s1: String = String::from("Hello");
+    let s2: String = String::from("wrold!");
+    let s3: String  = s1 + &s2; // s1 was moved to s3 and not exited, String type must be first
+    let s3: String = s1 + "test";
+    let s3: String  = format!("{}{}", s1, s2);
+    let s3: String  =["first", "sceond"].concat();
+    
+    let s3: &str = concat!("first", "second");
+
+    // index
+    let s1: &str = "😀the😀😀"; //
+    let s2: &str = &s1[0..4];  // first smile face
+
+    for b in "😀the😀😀".bytes() {
+
+    } 
+
+    for b in "😀the😀😀".chars() {
+
+    } 
 }
 
 
 pub fn first_line(string: &str) -> &str {
-  string.lines().next().unwrap();
+  string.lines().next().unwrap()
 } 
