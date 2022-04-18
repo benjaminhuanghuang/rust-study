1d8e7a48129511c4fb264f426cb8a8824b65c89c
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2022-04-18 09:11:14 -0700

    lifetime

--- #Lifetime/lifetime_demo/src/main.rs
+++ #Lifetime/lifetime_demo/src/main.rs
@@ -1,18 +1,29 @@
-/*
-  will be move to other scope by default
-*/
+#![allow(dead_code)]
 struct Hello {
   v: i32,
 }
-
+/*
+  will be move to other scope by default
+*/
 fn consume(h: Hello) {
   println!("{}", h.v)
 }
 
+/*
+  will be move to other scope by default
+*/
+fn consume_reference(h: &Hello) {
+  println!("{}", h.v)
+}
 
 fn main() {
   let h = Hello{ v:30};
+  
+
   println!("{}", h.v);
 
-  consume(h);
+  // consume(h);
+
+  consume_reference(&h);
+  consume_reference(&h);
 }
