6a23f10e7b8527c43f6cde86ae5903a1fda9bc65
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2022-04-19 20:53:13 -0700

    update

--- warp-tokio-api/src/main.rs
+++ warp-tokio-api/src/main.rs
@@ -1,12 +1,16 @@
 use warp::Filter;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+  env::set_var("RUST_APP_LOG", "debug");
+  pretty_env_logger::init_custom_env("RUST_APP_LOG");
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 8964))
-        .await;
+  //let log = warp::log("basic");
+
+  // GET /hello/warp => 200 OK with body "Hello, warp!"
+  let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+  warp::serve(hello).run(([127, 0, 0, 1], 8964)).await;
 }
+  
