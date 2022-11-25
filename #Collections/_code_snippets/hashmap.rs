e4972bf3848579367ae15b9a890b18bbd61e39f3
BenjaminHuang <benjaminhuanghuang@gmail.com>
2022-11-25 10:17:31 -0800

    update

--- #Collections/_code_snippets/hashmap.rs
+++ #Collections/_code_snippets/hashmap.rs
@@ -9,59 +9,68 @@ enum Fruit {
   Pineapple,
 }
 
-fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
-  let fruit_kinds = vec![
-    Fruit::Apple,
-    Fruit::Banana,
-    Fruit::Mango,
-    Fruit::Lychee,
-    Fruit::Pineapple,
-  ];
+//------------------------------------------------
+// Create
+//------------------------------------------------
+let mut basket = HashMap::<Fruit, u32>::new();
+let mut basket = HashMap::new();
+let mut scores: HashMap<String, u32> = HashMap::new();
 
-  for fruit in fruit_kinds {
-    // TODO: Put new fruits if not already present. Note that you
-    // are not allowed to put any type of fruit that's already
-    // present!
-    basket.insert(Fruit::Pineapple, 10);
-    basket.insert(Fruit::Banana, 10);
+
+//------------------------------------------------
+// Insert
+//------------------------------------------------
+basket.insert(Fruit::Apple, 4);
+basket.insert(Fruit::Mango, 2);
+basket.insert(Fruit::Lychee, 5);
+
+
+//------------------------------------------------
+// Get entry, Insert / Modify
+//------------------------------------------------
+let r = "England,France,4,2";
+let v: Vec<&str> = r.split(',').collect();
+let team_1_name = v[0].to_string();
+let team_1_score: u8 = v[2].parse().unwrap();
+let team_2_name = v[1].to_string();
+let team_2_score: u8 = v[3].parse().unwrap();
+
+let t1 = scores.entry(team_1_name.clone()).or_insert(Team {
+  name: team_1_name,
+  goals_scored: 0,
+  goals_conceded: 0,
+});
+(*t1).goals_scored += team_1_score;
+(*t1).goals_conceded += team_2_score;
+
+let t2 = scores.entry(team_2_name.clone()).or_insert(Team {
+  name: team_2_name,
+  goals_scored: 0,
+  goals_conceded: 0,
+});
+(*t2).goals_conceded += team_1_score;
+(*t2).goals_scored += team_2_score;
+
+//------------------------------------------------
+// Count
+//------------------------------------------------
+let count_fruit_kinds = basket.len();
+
+//------------------------------------------------
+// Check
+//------------------------------------------------
+let fruit_kinds = vec![Fruit::Lychee,Fruit::Mango]
+for fruit in fruit_kinds {
+  if !basket.contains_key(&fruit) {
+      basket.insert(fruit, 10);
   }
 }
+//------------------------------------------------
+// Values
+//------------------------------------------------
 
-#[cfg(test)]
-mod tests {
-  use super::*;
 
-  fn get_fruit_basket() -> HashMap<Fruit, u32> {
-    let mut basket = HashMap::<Fruit, u32>::new();
-    basket.insert(Fruit::Apple, 4);
-    basket.insert(Fruit::Mango, 2);
-    basket.insert(Fruit::Lychee, 5);
-
-    basket
-  }
-
-  #[test]
-  fn test_given_fruits_are_not_modified() {
-    let mut basket = get_fruit_basket();
-    fruit_basket(&mut basket);
-    assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
-    assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
-    assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
-  }
-
-  #[test]
-  fn at_least_five_types_of_fruits() {
-    let mut basket = get_fruit_basket();
-    fruit_basket(&mut basket);
-    let count_fruit_kinds = basket.len();
-    assert!(count_fruit_kinds >= 5);
-  }
-
-  #[test]
-  fn greater_than_eleven_fruits() {
-    let mut basket = get_fruit_basket();
-    fruit_basket(&mut basket);
-    let count = basket.values().sum::<u32>();
-    assert!(count > 11);
-  }
-}
+//------------------------------------------------
+// Key-value
+//------------------------------------------------
+assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
