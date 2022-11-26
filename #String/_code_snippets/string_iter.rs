4cb4605e2053d7c2619bff11630ebba8c2517634
BenjaminHuang <benjaminhuanghuang@gmail.com>
2022-11-26 15:55:16 -0800

    update

---
+++ #String/_code_snippets/string_iter.rs
@@ -0,0 +1,45 @@
+// "hello" -> "Hello"
+pub fn capitalize_first(input: &str) -> String {
+  let mut c = input.chars();
+  match c.next() {
+    None => String::new(),
+    Some(first) => first.to_uppercase().to_string() + c.as_str(),
+  }
+}
+
+// ["hello", "world"] -> ["Hello", "World"]
+pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+  words.iter().map(|x| capitalize_first(x)).collect()
+}
+
+// ["hello", " ", "world"] -> "Hello World"
+pub fn capitalize_words_string(words: &[&str]) -> String {
+  words.iter().map(|x| capitalize_first(x)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_success() {
+    assert_eq!(capitalize_first("hello"), "Hello");
+  }
+
+  #[test]
+  fn test_empty() {
+    assert_eq!(capitalize_first(""), "");
+  }
+
+  #[test]
+  fn test_iterate_string_vec() {
+    let words = vec!["hello", "world"];
+    assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+  }
+
+  #[test]
+  fn test_iterate_into_string() {
+    let words = vec!["hello", " ", "world"];
+    assert_eq!(capitalize_words_string(&words), "Hello World");
+  }
+}
