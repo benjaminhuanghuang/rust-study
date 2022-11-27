263f75f7e32bd7ff4d659f86d38d090518678f3e
BenjaminHuang <benjaminhuanghuang@gmail.com>
2022-11-26 22:03:02 -0800

    update

---
+++ #Collections/_code_snippets/vec_loop.rs
@@ -0,0 +1,29 @@
+
+let nums = vec![4, 3, 2, 7, 8, 2, 3, 1];
+
+
+/*
+  Use index
+*/
+for i in 0..nums.len() {
+  
+}
+
+for it in nums.iter() {
+  if *it < 10 {
+
+  } 
+}
+
+for (i, v) in nums.iter().enumerate() {
+    
+}
+
+/*
+  Skip
+*/
+for (j, num2) in nums.iter().skip(i + 1).enumerate() {
+  
+}
+
+
