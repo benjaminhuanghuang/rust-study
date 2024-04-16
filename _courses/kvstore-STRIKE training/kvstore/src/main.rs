8a8c3153e71c6ffaf94aad11392bf94331102fb4
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2024-04-16 15:37:25 -0700

    update

--- _courses/kvstore-STRIKE training/kvstore/src/main.rs
+++ _courses/kvstore-STRIKE training/kvstore/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() -> Result<(), std::io::Error> {
   let mut arguments = std::env::args().skip(1);
 
@@ -10,18 +12,38 @@ fn main() -> Result<(), std::io::Error> {
   // println!("Key: {}, Value: {}", key.unwrap(), value.unwrap());
   let strKey = key.unwrap();
   let strValue = value.unwrap();
-  write_database(strKey, strValue)
+
+  let mut db = Database::from_disk()?;
+  db.insert(strKey, strValue);
+  db.flush()?;
+
+  Ok(())
 }
 
 struct Database {
-  hashmap: std::collections::HashMap<String, String>,
+  hashmap: HashMap<String, String>,
 }
 
 impl Database {
-  fn from_disk() -> Database {
-    Database {
-      hashmap: std::collections::HashMap::new(),
+  fn from_disk() -> Result<Database, std::io::Error> {
+    let contents = std::fs::read_to_string("kv.db")?;
+    let mut hashmap = HashMap::new();
+    for lin in contents.lines() {
+      let mut key_value = lin.splitn(2, ' ');
+      let key = key_value.next().expect("No key");
+      let value = key_value.next().expect("No value");
+      hashmap.insert(key.to_owned(), value.to_owned());
     }
+    Ok(Database { hashmap })
+  }
+
+  fn insert(&mut self, key: String, value: String) {
+    self.hashmap.insert(key, value);
+  }
+
+  fn flush(&self) -> Result<(), std::io::Error> {
+    let contents: String = todo!("Format the keys and values in self.hashmap and return a String");
+    std::fs::write("kv.db", contents)
   }
 }
 
