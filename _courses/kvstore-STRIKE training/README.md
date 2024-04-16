b122328ab27cf9cb8f59491ba0a600452df931b7
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2024-04-16 00:11:50 -0700

    update

--- _courses/kvstore-STRIKE training/README.md
+++ _courses/kvstore-STRIKE training/README.md
@@ -1,10 +1,27 @@
 # STRIKE training
+
 https://eng.ms/docs/more/rust/intro/getting-started
 
 https://learningplayer.microsoft.com/activity/s9202352/launch
 https://strikecommunity.azurewebsites.net/articles/8612/course-introduction-to-the-rust-programming-langua-1.html
 
 Create project
-```sh 
+
+```sh
 cargo new kystore
+```
+
+Build project
+
+```sh
+cargo check 
+cargo build
+```
+
+Run project
+
+```sh
+.\target\debug\kvstore.exe
+or
+cargo run -- arg1 arg2
 ```
--- _courses/kvstore-STRIKE training/kvstore/src/main.rs
+++ _courses/kvstore-STRIKE training/kvstore/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-  println!("Hello, world!");
+  let mut arguments = std::env::args().skip(1);
+
+  // for arg in arguments {
+  //   println!("Got arg:{}", arg);
+  // }
+  let key = arguments.next(); // return an Option<String>
+  let value = arguments.next();
+  // println!("Key: {:?}, Value: {:?}", key, value);
+  // println!("Key: {}, Value: {}", key.unwrap(), value.unwrap());
+
+  let write_result = std::fs::write("kv.db", "Hello world!");
+  match write_result {
+    Ok(()) => {
+      println!("Write successful");
+    }
+    Err(e) => {
+      println!("Write failed: {}", e);
+    }
+  }
 }
