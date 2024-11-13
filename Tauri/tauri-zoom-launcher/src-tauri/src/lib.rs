92e66a8b660cdcf55c84cee4b812b9cb10504449
benjanmin huang <benjaminhuanghuang@gmail.com>
2024-11-13 09:57:05 -0800

    zoom launcher

--- Tauri/tauri-zoom-launcher/src-tauri/src/lib.rs
+++ Tauri/tauri-zoom-launcher/src-tauri/src/lib.rs
@@ -1,15 +1,82 @@
+use serde::{Deserialize, Serialize};
 use std::fs;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-  format!("Hello, {}! You've been greeted from Rust!", name)
+#[cfg(target_os = "macos")]
+const APPLICATION_DIRS: &[&str] = &["/Applications", "/Users/*/Applications"];
+
+#[cfg(target_os = "windows")]
+const APPLICATION_DIRS: &[&str] = &["C:\\Program Files", "C:\\Program Files (x86)"];
+
+#[cfg(target_os = "linux")]
+const APPLICATION_DIRS: &[&str] = &["/usr/bin", "/usr/local/bin", "/opt"];
+
+#[derive(Serialize, Deserialize)]
+struct RunCommandResult {
+  is_success: bool,
+  information: Vec<String>,
 }
 
-
+/*
+  Load the configuration file from the given path
+*/
 #[tauri::command]
-fn check_config_file() -> bool {
-    fs::metadata(path).is_ok()
+fn load_configuration() -> RunCommandResult {
+  let mut result = RunCommandResult {
+    is_success: false,
+    information: vec![],
+  };
+  result
+    .information
+    .push(format!("Run command: {}", "load_configuration"));
+  result
+}
+
+/*
+  Run the zoom client from installed package
+*/
+#[tauri::command]
+fn run_from_installed() -> RunCommandResult {
+  let mut result = RunCommandResult {
+    is_success: false,
+    information: vec![],
+  };
+  result
+    .information
+    .push(format!("Run command: {}", "run_from_installed"));
+
+  result
+}
+
+/*
+  Run the zoom client with local source code
+*/
+#[tauri::command]
+fn run_with_local_source() -> RunCommandResult {
+  let mut result = RunCommandResult {
+    is_success: false,
+    information: vec![],
+  };
+  result
+    .information
+    .push(format!("Run command: {}", "run_with_local_source"));
+
+  result
+}
+
+/*
+  Run the zoom client with local source and zoom bridge
+*/
+#[tauri::command]
+fn run_with_local_source_bridge() -> RunCommandResult {
+  let mut result = RunCommandResult {
+    is_success: false,
+    information: vec![],
+  };
+  result
+    .information
+    .push(format!("Run command: {}", "run_with_local_source_bridge"));
+
+  result
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -17,8 +84,10 @@ pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
     .invoke_handler(tauri::generate_handler![
-      greet, 
-      checkConfigFile
+      load_configuration,
+      run_from_installed,
+      run_with_local_source,
+      run_with_local_source_bridge
     ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
