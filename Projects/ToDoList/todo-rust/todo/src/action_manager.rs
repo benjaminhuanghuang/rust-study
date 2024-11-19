0921d55ea593afbc494f167ceca9df03302dae73
benjanmin huang <benjaminhuanghuang@gmail.com>
2024-11-18 23:16:53 -0800

    update

---
+++ Projects/ToDoList/todo-rust/todo/src/action_manager.rs
@@ -0,0 +1,175 @@
+use std::collections::HashMap;
+
+use crate::{
+  action_args::ActionArgs, display::DisplayMessage, repository::Repository,
+  task_manager::TaskManager,
+};
+
+type ActionHandler =
+  fn(am: &mut ActionManger, args: ActionArgs, display: &dyn DisplayMessage) -> bool;
+
+pub struct ActionManger {
+  actions: HashMap<&'static str, ActionHandler>,
+  manager: TaskManager,
+  repository: Box<dyn Repository>,
+}
+
+impl ActionManger {
+  pub fn new(repository: Box<dyn Repository>) -> Self {
+    let mut action_manager = Self {
+      actions: Self::actions_mapper(),
+      manager: TaskManager::new(),
+      repository,
+    };
+
+    action_manager.load();
+
+    action_manager
+  }
+
+  pub fn process(&mut self, args: ActionArgs, display: &dyn DisplayMessage) -> bool {
+    let command = args.command.clone().unwrap();
+
+    match self.select_action(command.as_str()) {
+      Ok(f) => f(self, args, display),
+      Err(_) => false,
+    }
+  }
+
+  fn select_action(&mut self, action: &str) -> Result<ActionHandler, &'static str> {
+    match self.actions.get(&action) {
+      Some(f) => Ok(*f),
+      None => Err("Command not found."),
+    }
+  }
+
+  fn actions_mapper() -> HashMap<&'static str, ActionHandler> {
+    let mut actions: HashMap<&'static str, ActionHandler> = HashMap::new();
+
+    actions.insert("add", ActionManger::add);
+    actions.insert("display", ActionManger::display);
+    actions.insert("remove", ActionManger::remove);
+    actions.insert("update", ActionManger::update);
+    actions.insert("complete", ActionManger::complete);
+    actions.insert("save", ActionManger::save);
+
+    actions
+  }
+
+  fn load(&mut self) {
+    let tasks = self.repository.load();
+
+    self.manager.set_tasks(tasks);
+  }
+
+  fn add(&mut self, args: ActionArgs, _display: &dyn DisplayMessage) -> bool {
+    let name = args.first.unwrap();
+    let description = args.second.unwrap();
+
+    self.manager.add(name.as_str(), description.as_str())
+  }
+
+  fn display(&mut self, _args: ActionArgs, display: &dyn DisplayMessage) -> bool {
+    self.manager.get_tasks().iter().for_each(|task| {
+      let message = format!("{}", task);
+
+      display.show(message);
+    });
+
+    true
+  }
+
+  fn remove(&mut self, args: ActionArgs, _display: &dyn DisplayMessage) -> bool {
+    let id = args.first.unwrap().parse::<u32>().unwrap();
+    self.manager.remove_by(id)
+  }
+
+  fn update(&mut self, args: ActionArgs, _display: &dyn DisplayMessage) -> bool {
+    let id = args.third.unwrap().parse::<u32>().unwrap();
+
+    let name = args.first.unwrap();
+    let description = args.second.unwrap();
+
+    self
+      .manager
+      .update_by(id, name.as_str(), description.as_str())
+  }
+
+  fn complete(&mut self, args: ActionArgs, _display: &dyn DisplayMessage) -> bool {
+    let id = args.first.unwrap().parse::<u32>().unwrap();
+    self.manager.complete_by(id)
+  }
+
+  fn save(&mut self, _args: ActionArgs, _display: &dyn DisplayMessage) -> bool {
+    self.repository.save(self.manager.get_tasks_store())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::{action_args::ActionArgsBuilder, display::DisplayMessage, repository::Repository};
+
+  use super::ActionManger;
+
+  #[derive(Clone)]
+  struct Test;
+
+  impl Repository for Test {
+    fn save(&mut self, _tasks: Vec<crate::task::Task>) -> bool {
+      true
+    }
+
+    fn load(&mut self) -> Vec<crate::task::Task> {
+      Vec::new()
+    }
+  }
+
+  impl DisplayMessage for Test {
+    fn show(&self, _message: String) {}
+  }
+
+  #[test]
+  fn test_add() {
+    let test = Test;
+
+    let args = ActionArgsBuilder::new()
+      .with_command("add")
+      .with_first("Test".to_string())
+      .with_second("Description".to_string())
+      .build();
+
+    let mut action_manager = ActionManger::new(Box::new(test.clone()));
+
+    assert_eq!(action_manager.process(args, &test), true);
+  }
+
+  #[test]
+  fn test_add_name_empty() {
+    let test = Test;
+
+    let args = ActionArgsBuilder::new()
+      .with_command("add")
+      .with_first("".to_string())
+      .with_second("Description".to_string())
+      .build();
+
+    let mut action_manager = ActionManger::new(Box::new(test.clone()));
+
+    assert_eq!(action_manager.process(args, &test), false);
+  }
+
+  #[test]
+  fn test_add_description_empty() {
+    let test = Test;
+
+    let args = ActionArgsBuilder::new()
+      .with_command("add")
+      .with_first("Test".to_string())
+      .with_second("".to_string())
+      .build();
+
+    let mut action_manager = ActionManger::new(Box::new(test.clone()));
+
+    assert_eq!(action_manager.process(args, &test), false);
+  }
+}
--- Projects/ToDoList/todo-rust/todo/src/task_manager.rs
+++ Projects/ToDoList/todo-rust/todo/src/task_manager.rs
@@ -6,7 +6,7 @@ pub struct TaskManager {
 
 impl TaskManager {
   pub fn new() -> Self {
-    TaskManager { tasks: Vec::new() }
+    Self { tasks: Vec::new() }
   }
 
   pub fn add(&mut self, name: &str, description: &str) -> bool {
@@ -15,44 +15,46 @@ impl TaskManager {
         self.tasks.push(task);
         true
       }
+
       Err(_) => false,
     }
   }
 
-  pub fn get_by(&self, id: i32) -> Result<&Task, &'static str> {
-    match self.tasks.get(id as usize) {
+  pub fn get_by(&mut self, index: u32) -> Result<&Task, &'static str> {
+    match self.tasks.get(index as usize) {
       Some(task) => Ok(task),
-      None => Err("Task not found"),
+      None => Err("Not found"),
     }
   }
 
-  pub fn complete_by(&mut self, id: i32) -> bool {
-    for task in &mut self.tasks {
-      if task.id == id {
+  pub fn complete_by(&mut self, id: u32) -> bool {
+    self
+      .tasks
+      .iter_mut()
+      .find(|task| task.id == id)
+      .map(|task| {
         task.set_done(true);
-        return true;
-      }
-    }
-    false
+        true
+      })
+      .unwrap_or(false)
   }
 
-  pub fn update(&mut self, id: i32, name: &str, description: &str) -> bool {
-    for task in &mut self.tasks {
-      if task.id == id {
-        match task.update(name, description) {
-          Ok(_) => return true,
-          Err(_) => return false,
-        }
-      }
-    }
-    false
+  pub fn update_by(&mut self, id: u32, name: &str, description: &str) -> bool {
+    self
+      .tasks
+      .iter_mut()
+      .find(|task| task.id == id)
+      .map(|task| task.update(name, description).is_ok())
+      .unwrap_or(false)
   }
 
-  pub fn remove_by(&mut self, id: i32) -> bool {
+  pub fn remove_by(&mut self, id: u32) -> bool {
     let len = self.tasks.len();
+
     if let Some(position) = self.tasks.iter().position(|task| task.id == id) {
       self.tasks.remove(position);
     }
+
     len > self.tasks.len()
   }
 
@@ -64,8 +66,13 @@ impl TaskManager {
     &self.tasks
   }
 
+  pub fn get_tasks_store(&self) -> Vec<Task> {
+    self.tasks.clone()
+  }
+
   pub fn set_tasks(&mut self, tasks: Vec<Task>) {
     self.tasks = tasks;
+
     if let Some(value) = self.tasks.iter().max_by_key(|task| task.id) {
       set_counter(value.id.clone() as u32);
     }
