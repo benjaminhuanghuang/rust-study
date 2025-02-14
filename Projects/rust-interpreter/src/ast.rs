5f3ae45bfac9ab90a7ff4ea075a185f030c22dc4
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-02-13 20:15:41 -0800

    parsing

--- Projects/rust-interpreter/src/ast.rs
+++ Projects/rust-interpreter/src/ast.rs
@@ -1,3 +1,5 @@
+use core::str;
+
 use crate::token;
 
 trait Node {
@@ -23,4 +25,80 @@ impl Node for StatementNode {
 }
 enum ExpressionNode {
   Identifier,
+}
+
+impl Node for ExpressionNode {
+  fn token_literal(&self) -> String {
+    match self {
+      ExpressionNode::Identifier => "identifier".to_string(),
+    }
+  }
+
+  fn print_string(&self) -> String {
+    match self {
+      ExpressionNode::Identifier => "identifier".to_string(),
+    }
+  }
+}
+
+struct Program {
+  statements: Vec<StatementNode>,
+}
+
+impl Node for Program {
+  fn token_literal(&self) -> String {
+    if self.statements.len() > 0 {
+      self.statements[0].token_literal()
+    } else {
+      "".to_string()
+    }
+  }
+
+  fn print_string(&self) -> String {
+    let mut out = String::new();
+
+    for statement in self.statements.iter() {
+      out.push_str(statement.print_string().as_str());
+    }
+
+    out
+  }
+}
+
+struct LetStatement {
+  token: token::Token,
+  name: Identifier,
+  value: Option<Expression>,
+}
+
+impl Node for LetStatement {
+  fn token_literal(&self) -> String {
+    self.token.literal.clone()
+  }
+
+  fn print_string(&self) -> String {
+    let mut out = String::new();
+
+    out.push_str(self.token_literal().as_str());
+    out.push_str(" ");
+    out.push_str(self.name.value.as_str());
+    out.push_str(" = ");
+
+    if let Some(value) = &self.value {
+      out.push_str(value.print_string().as_str());
+    }
+
+    out.push_str(";");
+
+    out
+  }
+}
+struct Identifier {
+  token: token::Token,
+  value: String,
+}
+
+struct Expression {
+  token: token::Token,
+  value: ExpressionNode,
 }
